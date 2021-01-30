6088d02d28df6f7b86f87d6679e1660a018ebaa3
ourongxing <orongxing@gmail.com>
2021-01-30 14:55:34 +0800

    update

--- double_screen.sh
+++ double_screen.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output eDP-1 --mode 2160x1440 --pos 0x621 --rotate normal --output DP-1 --primary --mode 2560x1440 --pos 2160x0 --rotate normal --output HDMI-1 --off
+xrandr --output eDP-1 --mode 2160x1440 --pos 2560x707 --rotate normal --output DP-1 --primary --mode 2560x1440 --pos 0x0 --rotate normal --output HDMI-1 --off
